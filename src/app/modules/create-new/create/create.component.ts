c1707a6d74e7d84737770fbf06f4353035d199ba
Anurag Gaur <anuraggaur495@gmail.com>
2023-07-03 18:10:58 +0530

    Form layout complete

--- src/app/modules/create-new/create/create.component.ts
+++ src/app/modules/create-new/create/create.component.ts
@@ -15,11 +15,15 @@ export class CreateComponent implements OnInit {
   fumigationDosageOptions: string[] = ['48 g/m^3', '100 g/m^3', '200 g/m^3'];
   fumigationDurationOptions: string[] = ['24 hours', '48 hours', '72 hours'];
   isFumigationGastightOptions: string[] = ['Yes', 'No'];
+  packagingMaterialOptions: string[] = ['Pallet Packing', 'Plastic Packing', 'Wood Packing', 'Other'];
 
   // const pattern = /^(?:[A-Z]{4}[0-9]{7})(?:,(?:[A-Z]{4}[0-9]{7}))*$/; --> Regex pattern for comma delimited container numbers
 
   ngOnInit(): void {
     this.fumigationForm = this.fb.group({
+      treatmentCertificateNumber: [''],
+      brandName: [''],
+      dateOfIssue: [''],
       fumigantName: [''],
       fumigationDate: [''],
       fumigationPlace: [''],
@@ -34,12 +38,40 @@ export class CreateComponent implements OnInit {
       cargoDescription: [''],
       destinationPort: [''],
       invoiceNumber: [''],
+      cotainerHeight: [''],
+      containerWidth: [''],
+      materialQuantityInfo: this.fb.group({
+        noOfPallets: [''],
+        noOfBoxes: [''],
+        totalArea: [''],
+      }),
+      materialWeightInfo: this.fb.group({
+        netWeight: [''],
+        grossWeight: [''],
+      }),
+      packagingMaterial: [''],
     });
-    // this.fumigationForm.valueChanges.subscribe(console.log);
+    // this.fumigationForm.get('materialQuantityInfo.noOfPallets')?.valueChanges.subscribe(console.log);
   }
 
-  get fumigantName() {
-    return this.fumigationForm.get('fumigantName');
+  get noOfPallets() {
+    return this.fumigationForm.get('materialQuantityInfo.noOfPallets');
+  }
+
+  get noOfBoxes() {
+    return this.fumigationForm.get('materialQuantityInfo.noOfBoxes');
+  }
+
+  get totalArea() {
+    return this.fumigationForm.get('materialQuantityInfo.totalArea');
+  }
+
+  get netWeight() {
+    return this.fumigationForm.get('materialWeightInfo.netWeight');
+  }
+
+  get grossWeight() {
+    return this.fumigationForm.get('materialWeightInfo.grossWeight');
   }
 
 }
